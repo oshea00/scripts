8e2da3b1e8968fdaad06c7fa903c58485d18ff74
moshea <oshea00@gmail.com>
2020-02-17 05:23:18 +0000

    setup docker

---
+++ setupDocker.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg2
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker $USER
+
